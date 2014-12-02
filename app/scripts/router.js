5e8221ac61a097af45eb3eb3c90ce01eccdea2e4
Restuta <restuta8@gmail.com>
2014-12-01 17:20:48 -0800

    defined routes for listings and listings/:id, added template to show list of listings when going to #/listings route

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,3 +1,27 @@
-EmberApp.Router.map(function () {
-  // Add your routes here
+EmberApp.Router.map(function() {
+    this.resource('listings', function() {
+        this.resource('listing', {path: ':listing_id'});
+    });
+});
+
+var listings = [{
+    id: 1,
+    name: 'First listing',
+    minuteRate: 2.99
+}, {
+    id: 2,
+    name: 'Second listing',
+    minuteRate: 5.88
+}];
+
+EmberApp.ListingsRoute = Ember.Route.extend({
+    model: function() {
+        return listings;
+    }
+});
+
+EmberApp.ListingRoute = Ember.Route.extend({
+    model: function(params) {
+        return listings.findBy('id', params.listing_id);
+    }
 });
