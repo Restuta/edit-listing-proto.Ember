1bd60c69f75c884866a6b6f48e8f1d1f0fc971c4
Restuta <restuta8@gmail.com>
2014-12-08 22:24:12 -0800

    renamed everything to drafts

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -3,16 +3,17 @@ App.Router.map(function() {
         this.route('new');
     });
 
-    this.resource('listing', {path: 'listings/:listing_id'});
-    this.resource('drafts', {path: 'listings/drafts/:draft_id'});
+    this.resource('drafts', {path: 'listings/drafts/'});
+    this.resource('draft', {path: 'listings/drafts/:draft_id'});
 
     this.resource('about');
 });
 
 //note: model should NOT define id or things will break
-App.Listing = DS.Model.extend({
+App.Draft = DS.Model.extend({
     name: DS.attr(),
     minuteRate: DS.attr(),
+    //todo: these properties can also be put in controller, we need to figure out good reasons to favor one over another
     ourFee: function() {
         return (this.get('minuteRate') * 0.3).toFixed(2);
     }.property('minuteRate'),
@@ -21,18 +22,7 @@ App.Listing = DS.Model.extend({
     }.property('minuteRate')
 });
 
-App.ListingDraft = DS.Model.extend({
-    name: DS.attr(),
-    minuteRate: DS.attr(),
-    ourFee: function() {
-        return (this.get('minuteRate') * 0.3).toFixed(2);
-    }.property('minuteRate'),
-    youWillEarn: function() {
-        return (this.get('minuteRate') - this.get('ourFee')).toFixed(2);
-    }.property('minuteRate')
-});
-
-App.ListingDraft.FIXTURES = [{
+App.Draft.FIXTURES = [{
     id: 1,
     name: 'First listing',
     minuteRate: 2.99
@@ -42,28 +32,10 @@ App.ListingDraft.FIXTURES = [{
     minuteRate: 5.88
 }];
 
-App.Listing.FIXTURES = [{
-    id: 1,
-    name: 'First listing',
-    minuteRate: 2.99
-}, {
-    id: 2,
-    name: 'Second listing',
-    minuteRate: 5.88
-}];
-
-App.ListingsRoute = Ember.Route.extend({
+App.DraftsRoute = Ember.Route.extend({
     model: function() {
-        return this.store.findAll('listing');
-    }
-});
-
-App.ListingRoute = Ember.Route.extend({
-    model: function(params) {
-        //todo: this is not getting executed for some reason, figure out why
-        //but it's getting kicked when we come directly to that route, not by clicking on a link at 'listings/' page
-        //debugger;
-        return this.store.find('listing', params.listing_id);
+        console.log('drafts route: store.findAll');
+        return this.store.findAll('draft');
     }
 });
 
@@ -71,26 +43,31 @@ App.ListingsNewRoute = Ember.Route.extend({
     redirect: function() {
         console.log('POST /listings/drafts/ and get its id');
         var draftId = 2;
-        this.transitionTo('drafts', draftId);
+        this.transitionTo('draft', draftId);
     }
 });
 
-App.DraftsRoute = Ember.Route.extend({
-    //todo: fetching of the model by id might be a default thing in Ember, check that
+App.DraftRoute = Ember.Route.extend({
+    //todo: fetching of the model by id is a default thing in Ember
     model: function(params) {
-        console.log('DraftsRoute');
-        return this.store.find('listingDraft', params.draft_id);
+        console.log('draft route');
+        //todo: this is not getting executed for some reason, figure out why
+            //but it's getting kicked when we come directly to that route, not by clicking on a link at 'listings/' page
+        return this.store.find('draft', params.draft_id);
     }
 });
 
-App.ListingsController = Ember.ArrayController.extend({
+App.DraftsController = Ember.ArrayController.extend({
     total: function() {
         return this.get('length');
     }.property('length')
 });
 
-App.ListingController = Ember.ObjectController.extend({
+App.DraftController = Ember.ObjectController.extend({
     //ourFee: function() {
-    //    return this.get('model.minuteRate') * 0.3;
+    //    return (this.get('model.minuteRate') * 0.3).toFixed(2);
+    //}.property('model.minuteRate'),
+    //youWillEarn: function() {
+    //    return (this.get('model.minuteRate') - this.get('ourFee')).toFixed(2);
     //}.property('model.minuteRate')
 });
