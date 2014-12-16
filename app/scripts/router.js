afca48c8e15f08ce9fd1435d088c6173ed9edbe6
Restuta <restuta8@gmail.com>
2014-12-16 14:53:30 -0800

    replaced transitionTo( on replaceWith( ro make sure /lisgings/new is not getting stored in browser history

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -33,7 +33,7 @@ App.ListingsNewRoute = Ember.Route.extend({
         var route = this;
         newDraft.save().then(function() {
             //todo: this makes "back" button to go to /listing/new, which is no desired
-            route.transitionTo('draft', newDraft);
+            route.replaceWith('draft', newDraft);
         });
     }
 });
